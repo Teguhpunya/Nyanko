c363800afd8927859bfcedc4f667d22859287ae9
Teguhpunya <teguhpunya.git@gmail.com>
2020-06-29 08:53:52 +0700

    help: sort modules
    Signed-off-by: Teguhpunya <teguhpunya.git@gmail.com>

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -19,10 +19,14 @@ async def help(event):
         else:
             await event.edit("Please specify a valid module name.")
     else:
-        await event.edit("Please specify which module do you want help for !!"
-                         "\nUsage: .help <module name>")
-        string = "-  "
+        await event.edit(
+            "Please specify which module do you want help for !!"
+            "\nUsage: .help <module name>"
+        )
+        modules = []
+        result = "`"
         for i in CMD_HELP:
-            string += "`" + str(i)
-            string += "`  -  "
-        await event.reply(string)
+            modules.append(str(i))
+        result += "` - `".join(map(str, sorted(modules)))
+        result += "`"
+        await event.reply(result)
